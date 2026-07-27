4ccbf25f6b7956e8bbb7326b6a61f1f4aa3a644d
Serkut Yıldırım <71320243+serkutYILDIRIM@users.noreply.github.com>
2026-07-27 23:56:31 +0300

    Update UnauthorizedExceptionTests.cs

--- openbanking-mobile-bff.Tests/Common/Exceptions/UnauthorizedExceptionTests.cs
+++ openbanking-mobile-bff.Tests/Common/Exceptions/UnauthorizedExceptionTests.cs
@@ -9,6 +9,7 @@ public sealed class UnauthorizedExceptionTests
     public void Constructor_WithMessage_SetsStatusCodeToUnauthorized()
     {
         var exception = new UnauthorizedException("Access denied.");
+        
         Assert.Equal(HttpStatusCode.Unauthorized, exception.StatusCode);
     }
 
