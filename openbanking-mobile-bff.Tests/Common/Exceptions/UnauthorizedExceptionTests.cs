c8bfbe2206095832cd0fbe345bcf4c171b9d328d
Serkut Yıldırım <71320243+serkutYILDIRIM@users.noreply.github.com>
2026-07-29 23:57:02 +0300

    Update UnauthorizedExceptionTests.cs

--- openbanking-mobile-bff.Tests/Common/Exceptions/UnauthorizedExceptionTests.cs
+++ openbanking-mobile-bff.Tests/Common/Exceptions/UnauthorizedExceptionTests.cs
@@ -36,6 +36,7 @@ public sealed class UnauthorizedExceptionTests
         var exception = new UnauthorizedException("Access denied.");
 
         Assert.IsAssignableFrom<OhvpsException>(exception);
+        
         Assert.IsAssignableFrom<Exception>(exception);
     }
 
