5a06d7f147d3a179fbb63aec6de9b0468319d801
Serkut Yıldırım <71320243+serkutYILDIRIM@users.noreply.github.com>
2026-07-30 23:53:24 +0300

    Update CardController.cs

--- openbanking-mobile-bff/Controllers/CardController.cs
+++ openbanking-mobile-bff/Controllers/CardController.cs
@@ -22,6 +22,7 @@ public sealed class CardController : ControllerBase
         [FromHeader(Name = "X-TPP-Code")] string tppCode)
     {
         var result = await _cardService.GetCardsAsync(requestId, aspspCode, tppCode);
+        
         return Ok(result);
     }
 
