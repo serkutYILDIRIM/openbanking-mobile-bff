de29e16060ec4319347531ab0bea5f9016dd27fe
Serkut Yıldırım <71320243+serkutYILDIRIM@users.noreply.github.com>
2026-09-14 23:54:26 +0300

    Update HttpClientExtensionsTests.cs

--- openbanking-mobile-bff.Tests/Extensions/HttpClientExtensionsTests.cs
+++ openbanking-mobile-bff.Tests/Extensions/HttpClientExtensionsTests.cs
@@ -41,7 +41,6 @@ public sealed class HttpClientExtensionsTests
     {
         var httpClientField = client.GetType().GetField("_httpClient", BindingFlags.Instance | BindingFlags.NonPublic);
         Assert.NotNull(httpClientField);
-
         var httpClient = httpClientField!.GetValue(client) as HttpClient;
         Assert.NotNull(httpClient);
 
